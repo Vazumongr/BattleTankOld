673e149fad2e5ea9153bd6295191aa15a2a0a179
Troy Records Jr <giants2110@gmail.com>
2020-04-22 17:55:07 -0400

    Lecture 259 End

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -13,14 +13,12 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-
-	auto TankName = GetName();
 }
 
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();
-	auto TankName = GetName();
+	TankAimingComponent = FindComponentByClass<UTankAimingComponent>();
 }
 
 void ATank::AimAt(FVector HitLocation)
