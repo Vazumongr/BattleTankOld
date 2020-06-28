b5a7809d8e08809a6d4e20149ca519f6765ab447
Troy Records Jr <giants2110@gmail.com>
2020-06-28 18:46:22 -0400

    Attachment, Actors, and Components End.

--- BattleTank/Source/BattleTank/Private/SprungWheel.cpp
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -10,14 +10,16 @@ ASprungWheel::ASprungWheel()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Physics Constraint"));
+	SetRootComponent(PhysicsConstraint);
+
 	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
-	SetRootComponent(Mass);
+	Mass->SetupAttachment(RootComponent);
 
 	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
 	Wheel->SetupAttachment(RootComponent);
 
-	PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Physics Constraint"));
-	PhysicsConstraint->SetupAttachment(RootComponent);
 
 
 }
@@ -26,7 +28,15 @@ ASprungWheel::ASprungWheel()
 void ASprungWheel::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (GetAttachParentActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Not Null"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Null"));
+	}
 }
 
 // Called every frame
